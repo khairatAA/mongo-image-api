c26b12d8cba9d1ecd7c5badb6233060f38d4f03c
Khairat Adesina <khairatadesina01@gmail.com>
2024-01-18 22:44:22 +0100

    add: handles connection with database

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+// index module
+// Makes a connection to the database within a specific port
+
+import express from 'express';
+import mongoose from 'mongoose';
+import imageRoutes from './routes/imageRoutes';
+import cors from 'cors';
+import helmet from 'helmet';
+
+const mongoURI = 'mongodb://172.22.16.1:27017/image_api'
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Creates an express app
+app.use(express.json());
+app.use('/images', imageRoutes);
+app.use(cors()); // For Cross-Origin Resource Sharing
+app.use(helmet()); // For enchanced security
+
+// Connect to the MongoDB database
+mongoose.connect(mongoURI);
+
+// Debugging statement to ensure the connect to the database was seamless
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+db.once('open', () => {
+    console.log('Successfully connected to MongoDB');
+});
+
+// Debugging statement to shoe server is running
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
